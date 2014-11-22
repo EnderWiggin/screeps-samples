a7d0d871d59dad58850582d5878b7a018092efcf
EnderWiggin <andrii.gook@gmail.com>
2014-11-22 23:30:02 +0200

    simplified role filter

--- filter.js
+++ filter.js
@@ -1,4 +1,4 @@
 module.exports = {
-    role:function (role){ return {filter:function(creep){ return creep.memory.role === role; }}; },
+    role:function (role){ return {filter : {memory : {role : role}}}; },
     damaged:{filter:function (creep){ return creep.hits < creep.hitsMax; }}
 };
